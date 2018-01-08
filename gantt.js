c23a28a61af27c40a525bee394b2e073e07e0b82
caizkun <caizkun@gmail.com>
2018-01-08 17:35:57 -0600

    add an example image

--- gantt.js
+++ gantt.js
@@ -18,19 +18,19 @@ function drawChart() {
     // change to add/delete tasks
     data.addRows([
                   ['Expt.INS', 'INS Experiments', 'expt',
-                   new Date(2017, 12, 1), new Date(2018, 5, 20), null, 0, null],
+                   new Date(2017, 12, 1), new Date(2018, 5, 20), null, 33, null],
                   ['Expt.IXS', 'IXS Experiments', 'expt',
                    new Date(2018, 5, 15), new Date(2018, 8, 20), null, 0, null],
                   ['MD.simulation', 'MD Simulations', 'MD',
-                   new Date(2018, 5, 30), new Date(2018, 10, 10), null, 0, null],
+                   new Date(2018, 5, 30), new Date(2018, 10, 10), null, 67, null],
                   ['MD.density_corr', 'Characterization of Density Corerlations', 'MD',
-                   new Date(2018, 9, 10), new Date(2018, 12, 25), null, 0, 'MD.simulation'],
+                   new Date(2018, 9, 10), new Date(2018, 12, 25), null, 33, 'MD.simulation'],
                   ['MD.current_corr', 'Characterization of Current Corerlations', 'MD',
-                   new Date(2018, 9, 20), new Date(2019, 1, 25), null, 0, 'MD.simulation'],
+                   new Date(2018, 9, 20), new Date(2019, 1, 25), null, 67, 'MD.simulation'],
                   ['Theory.viscoelasticity', 'VH Theory: Viscoelastic Response', 'theory',
-                   new Date(2018, 10, 18), new Date(2019, 4, 5), null, 0, null],
+                   new Date(2018, 10, 18), new Date(2019, 4, 5), null, 50, null],
                   ['Theory.coupling', 'VH Theory: Longitudinal-Transverse Coupling', 'theory',
-                   new Date(2019, 4, 10), new Date(2019, 12, 25), null, 0, 'Theory.viscoelasticity']
+                   new Date(2019, 4, 10), new Date(2019, 12, 25), null, 50, 'Theory.viscoelasticity']
                   ]);
     
     // change options to customize the plot
